31a823bc2d96620cd9145fc68d2981a0babfaa25
Joshua Acklen <joshuaacklen@gmail.com>
2026-09-09 19:47:44 -0400

    Revert "Add /test deployment-check page"
    This reverts commit 32535c1492091b26b65e30377a2f4eaa87080221.

--- worker.js
+++ worker.js
@@ -3239,7 +3239,6 @@ export default {
       '/announcements': '/announcements',
       '/contact': '/contact',
       '/student-hub': '/student-hub',
-      '/test': '/test',
     };
 
     // topic/:id — any path matching /topic/<something>
